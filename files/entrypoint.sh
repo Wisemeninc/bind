42792287ec56cc8b2062e5a96838eb530bddf7b4
Peter Hamborg Haugaard <haugaard@wisemeninc.net>
2024-05-31 14:08:49 +0200

    Update entrypoint.sh

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 if [ ! -f /etc/bind/named.conf ]; then
     mkdir -p /var/cache/bind
-    chown -R named /var/cache/bind
+    chown -R bind /var/cache/bind
     cp -a etc_bind/* /etc/bind
 fi
 
-named -c /etc/bind/named.conf -g -u named
+named -c /etc/bind/named.conf -g -u bind
